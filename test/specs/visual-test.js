f014e8c2bc0307d873f6f5d9390c272d04300b37
Saabbir Hossain <thesaabbir@gmail.com>
2024-08-29 22:23:24 +0600

    Update test

--- test/specs/visual-test.js
+++ test/specs/visual-test.js
@@ -5,10 +5,17 @@ describe("Visual test", () => {
   it("should match homepage", async () => {
     await browser.url(argv.url);
 
-    await browser.setWindowPosition(0, 0);
-    // await browser.execute(() => {
-    //   window.scrollTo(0, 0);
-    // });
+    await browser.waitUntil(
+      () => browser.execute(() => document.readyState === "complete"),
+      {
+        timeout: 10000, // 10 seconds timeout
+        timeoutMsg: "Page did not load in time",
+      }
+    );
+
+    await browser.execute(() => {
+      window.scrollTo(0, 0);
+    });
 
     // await browser.checkScreen(argv.tag, {}); // Checks viewport only
     await browser.checkFullPageScreen(argv.tag, {});
